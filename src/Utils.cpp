31caa1f3b11a093dbf5d36382898fab838225717
Tuman <caedusle@school21.myguest.virtualbox.org>
2024-01-25 18:56:54 +0300

    color_out

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,20 +2,24 @@
 #include <string>
 #include <iostream>
 
-enum class Color: int {
-	RED      = 31,
-	GREEN    = 32,
-	YELLOW   = 33,
-	BLUE     = 34,
-	DEFAULT  = 39,
-};
+namespace Color {
+	enum color{
+		RED      = 31,
+		GREEN    = 32,
+		YELLOW   = 33,
+		BLUE     = 34,
+		DEFAULT  = 39,
+	};
 
-void colour_out(Color col, std::string str) {
-	std::cout << "\033[1;"<<static_cast<int>(col)<<"m"<< str << "\033[0m\n";
-
+	void colour_out(color col, std::string * str) {
+	std::cout << "\033[1;"<<static_cast<int>(col)<<"m"<< * str << "\033[0m\n";
+}
 }
 
-int main() {
-	colour_out(Color::YELLOW, "hut");
-	return 0;
-}
+
+
+// int main() {
+// 	std::string a = "tut";
+// 	colour_out(Color::BLUE, &a);
+// 	return 0;
+// }
